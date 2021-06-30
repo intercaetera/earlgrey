3acb7d7657500683165f10603828cd75f1e8631c
intercaetera <author@intercaetera.com>
2021-06-30 20:54:15 +0200

    Removed config template from gitignore

---
+++ config/_config.template.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :nostrum,
+  token: "<bot token here>",
+  num_shards: :auto
+
+config :earlgrey,
+  confirmation_thread: "<link to confirmation thread here>",
+  posting_channel_id: 0 # channel in which bot is supposed to post
